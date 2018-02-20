08274936d0e735885537c1a05f6812c676927c58
Vincent Rasquier <vrasquier@keley-live.com>
2018-02-20 17:59:40 +0100

    Add image to ddt

--- src/resources/cards/banals/ddt.card.ts
+++ src/resources/cards/banals/ddt.card.ts
@@ -5,7 +5,7 @@ import * as C from "../../../consts";
 class Ddt extends AbstractCard {
   uid = "ddt";
   name = "DDT";
-  img = "";
+  img = "https://pm1.narvii.com/5851/697f239d9cd6ec27e14c40b2b8b2ccd01bb9c6b6_hq.jpg";
   description = "";
   stamina = 3;
   intensity = 0;
